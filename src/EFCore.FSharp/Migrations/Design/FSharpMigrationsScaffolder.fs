db135cf8c8b44891c2281eeb3e94c9276493adb2
Durdsoft <durdsoft@gmail.com>
2020-07-12 17:17:19 +0100

    #43 PoC - Add new migrations to project

--- src/EFCore.FSharp/Migrations/Design/FSharpMigrationsScaffolder.fs
+++ src/EFCore.FSharp/Migrations/Design/FSharpMigrationsScaffolder.fs
@@ -1,10 +1,15 @@
 namespace EntityFrameworkCore.FSharp.Migrations.Design
 
+open System
+open System.IO
+open System.Text
 open Microsoft.EntityFrameworkCore.Migrations.Design
 open Microsoft.EntityFrameworkCore.Internal
 open EntityFrameworkCore.FSharp.IndentedStringBuilderUtilities
-open System.Text
-open System.IO
+open System
+open System
+open Microsoft.EntityFrameworkCore
+
 
 type FSharpMigrationsScaffolder(dependencies) = 
     inherit MigrationsScaffolder(dependencies)
@@ -45,6 +50,84 @@ type FSharpMigrationsScaffolder(dependencies) =
         dependencies.OperationReporter.WriteVerbose(DesignStrings.WritingSnapshot(modelSnapshotFile))
         Directory.CreateDirectory(modelSnapshotDirectory) |> ignore
         File.WriteAllText(modelSnapshotFile, migration.SnapshotCode, Encoding.UTF8)
+
+        let projectFiles =
+            Directory.GetFiles(projectDir)
+            |> Seq.filter (fun f -> f.EndsWith ".fsproj")
+            |> Seq.toList
+
+        match projectFiles with
+        | [] -> dependencies.OperationReporter.WriteVerbose(sprintf "Unable to find .fsproj file in %s" projectDir)
+
+        | [ projectFile ] ->
+            let projectDirectory = Path.GetDirectoryName projectFile
+            let projectContents = File.ReadAllLines projectFile
+
+            let allDbContextTypes =
+                dependencies.MigrationsAssembly.Assembly.GetTypes()
+                |> Seq.filter (fun t -> typeof<DbContext>.IsAssignableFrom t)
+                |> Seq.map (fun t -> sprintf "type %s" t.Name)
+            
+            let declaringFiles =
+                projectContents
+                |> Seq.filter (fun l -> l.Contains("<Compile Include=\""))
+                |> Seq.map (fun l -> l.Replace("<Compile Include=\"", "").Replace("/>", "").TrimStart().TrimEnd().TrimEnd('"'))
+                |> Seq.map (fun l -> (l, File.ReadAllText l))
+                |> Seq.filter (fun (_, c) -> allDbContextTypes |> Seq.exists (fun t -> c.Contains t))
+                |> Seq.map fst
+                |> Seq.toList
+
+            let rec calculateInsertPoint (compileIncludesToFind: string list) (searchLines: (int * string) seq) (acc: int) =
+                match compileIncludesToFind with
+                | [ ] -> acc + 1
+                | _ ->
+                    let line = compileIncludesToFind.Head
+                    let location =
+                        searchLines
+                        |> Seq.skipWhile (fun (_, l) -> l.Contains(sprintf "\"%s\"" line) |> not)
+                        |> Seq.map fst
+                        |> Seq.head
+                        
+                    let newLocation = if location > acc then location else acc
+                    calculateInsertPoint (compileIncludesToFind.Tail) searchLines newLocation
+
+            let indexedProjectContents =
+                projectContents
+                |> Seq.mapi (fun i l -> (i, l))
+
+            let insertionPoint =
+                calculateInsertPoint declaringFiles indexedProjectContents 0
+
+            let indentation =
+                (projectContents
+                |> Seq.toArray).[insertionPoint + 1]
+                |> Seq.takeWhile (fun c -> c = ' ')
+                |> String.Concat
+
+            let compileIncludes =
+                [ migrationFile; modelSnapshotFile; if migration.MigrationCode <> "// intentionally empty" then migrationMetadataFile; ]
+                |> Seq.filter (isNull >> not)
+                |> Seq.map (fun s -> s.Replace(projectDirectory, "").TrimStart('\\'))
+                |> Seq.map (fun s -> sprintf "%s<Compile Include=\"%s\" />" indentation s)
+                |> Seq.toList
+
+            let startProjectFile =
+                projectContents
+                |> Seq.take insertionPoint
+                |> Seq.toList
+
+            let endProjectFile =
+                projectContents
+                |> Seq.skip insertionPoint
+                |> Seq.toList
+
+            let newProjectContents =
+                [ startProjectFile; compileIncludes; endProjectFile ]
+                |> List.concat
+                
+            File.WriteAllLines(projectFile, newProjectContents)
+
+        | _ -> dependencies.OperationReporter.WriteVerbose(sprintf "Ambiguous .fsproj file in %s. Please manually add the generated files." projectDir)
 
         MigrationFiles (
             MigrationFile = migrationFile,
