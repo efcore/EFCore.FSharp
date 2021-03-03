6e1b2b6783c7400d4b5713f6f54d70fa9b80c39a
Simon Reynolds <github@simonreynolds.ie>
2021-03-03 16:05:18 +0000

    Add FSharpMigrationsScaffolderTest

--- src/EFCore.FSharp/Migrations/Design/FSharpMigrationsGeneratorDependencies.fs
+++ src/EFCore.FSharp/Migrations/Design/FSharpMigrationsGeneratorDependencies.fs
@@ -13,12 +13,3 @@ type FSharpMigrationsGeneratorDependencies
     member this.FSharpHelper = fSharpHelper
     member this.FSharpMigrationOperationGenerator = fSharpMigrationOperationGenerator
     member this.FSharpSnapshotGenerator = fSharpSnapshotGenerator
-
-    member this.With (fSharpHelper : ICSharpHelper) =
-        FSharpMigrationsGeneratorDependencies (fSharpHelper, this.FSharpMigrationOperationGenerator, this.FSharpSnapshotGenerator)
-
-    member this.With (fSharpMigrationOperationGenerator : ICSharpMigrationOperationGenerator) =
-        FSharpMigrationsGeneratorDependencies (this.FSharpHelper, fSharpMigrationOperationGenerator, this.FSharpSnapshotGenerator)
-
-    member this.With (fSharpSnapshotGenerator : ICSharpSnapshotGenerator) =
-        FSharpMigrationsGeneratorDependencies (this.FSharpHelper, this.FSharpMigrationOperationGenerator, fSharpSnapshotGenerator)
