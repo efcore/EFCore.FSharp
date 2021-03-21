b2427314a5158187e0fba5c1e25aa48c52324b8b
Simon Reynolds <github@simonreynolds.ie>
2021-03-21 18:00:38 +0000

    Update test AssemblyInfo

--- tests/EFCore.FSharp.Tests/AssemblyInfo.fs
+++ tests/EFCore.FSharp.Tests/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp.Tests")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyVersionAttribute("5.0.3")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-21T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("5.0.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("5.0.3")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","6fcff344c38a533e1a0c690cd85b30c9824455cd")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp.Tests"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyVersion = "5.0.3"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-21T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "5.0.3"
+    let [<Literal>] AssemblyInformationalVersion = "5.0.3"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
+    let [<Literal>] AssemblyMetadata_GitHash = "6fcff344c38a533e1a0c690cd85b30c9824455cd"
