3f894592b96f4eba29430d2ea9849671ba96e233
Simon Reynolds <github@simonreynolds.ie>
2021-03-21 14:35:39 +0000

    Add unit test for DbContextHelpers

--- src/EFCore.FSharp/DbContextHelpers.fs
+++ src/EFCore.FSharp/DbContextHelpers.fs
@@ -4,7 +4,7 @@ open Microsoft.EntityFrameworkCore
 
 /// Helper method for saving an updated record type
 let updateEntity (ctx: #DbContext) (key: 'a -> obj) (entity : 'a when 'a : not struct) =
-    let currentEntity = ctx.Set<'a>("").Find (key entity)
+    let currentEntity = ctx.Set<'a>().Find (key entity)
     ctx.Entry(currentEntity).CurrentValues.SetValues(entity :> obj)
     ctx.SaveChanges() |> ignore
     entity
