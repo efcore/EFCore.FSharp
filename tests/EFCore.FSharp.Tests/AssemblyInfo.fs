e56a910d733ebd46ae824844f5171ccd482781bf
Simon Reynolds <github@simonreynolds.ie>
2021-03-27 17:26:47 +0000

    Update AssemblyInfo

--- tests/EFCore.FSharp.Tests/AssemblyInfo.fs
+++ tests/EFCore.FSharp.Tests/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp.Tests")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyVersionAttribute("5.0.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-21T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-27T00:00:00.0000000+00:00")>]
 [<assembly: AssemblyFileVersionAttribute("5.0.3")>]
 [<assembly: AssemblyInformationalVersionAttribute("5.0.3")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","6fcff344c38a533e1a0c690cd85b30c9824455cd")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","6a88efe1225969c49050289148a4179c3080b0dc")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp.Tests"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
     let [<Literal>] AssemblyVersion = "5.0.3"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-21T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-27T00:00:00.0000000+00:00"
     let [<Literal>] AssemblyFileVersion = "5.0.3"
     let [<Literal>] AssemblyInformationalVersion = "5.0.3"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
-    let [<Literal>] AssemblyMetadata_GitHash = "6fcff344c38a533e1a0c690cd85b30c9824455cd"
+    let [<Literal>] AssemblyMetadata_GitHash = "6a88efe1225969c49050289148a4179c3080b0dc"
