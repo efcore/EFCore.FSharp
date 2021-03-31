01390ba081ad512a493b47adbe441bb16d24d745
Simon Reynolds <github@simonreynolds.ie>
2021-03-31 17:04:33 +0100

    Bump version to 5.0.3-alpha3
    ## [5.0.3-alpha3] - 2021-03-31
    
    ### Fixed
     - Generated Migrations will now always include the System namespace - https://github.com/efcore/EFCore.FSharp/pull/70
    
    - Removed unneeded dependency
    - Link to NuGet badges in README.md
    
    ### Added
    - DbContextHelpers - curried functions for interacting with DbContext to allow for a more 'native' F# experience
    - Initial release
    - F# migrations
    - F# scaffolding
    [Unreleased]: https://github.com/efcore/EFCore.FSharp/compare/v5.0.3-alpha1...HEAD
    [5.0.3-alpha1]: https://github.com/efcore/EFCore.FSharp/releases/tag/v5.0.3-alpha1
    - Initial release
    - F# migrations
    - F# scaffolding
    [Unreleased]: https://github.com/efcore/EFCore.FSharp/compare/v5.0.3-alpha2...HEAD
    [5.0.3-alpha2]: https://github.com/efcore/EFCore.FSharp/releases/tag/v5.0.3-alpha2
    [5.0.3-alpha1]: https://github.com/efcore/EFCore.FSharp/releases/tag/v5.0.3-alpha1

--- src/EFCore.FSharp/AssemblyInfo.fs
+++ src/EFCore.FSharp/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyVersionAttribute("5.0.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-27T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-03-31T00:00:00.0000000+01:00")>]
 [<assembly: AssemblyFileVersionAttribute("5.0.3")>]
 [<assembly: AssemblyInformationalVersionAttribute("5.0.3")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","6a88efe1225969c49050289148a4179c3080b0dc")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","0566eb7adf247cb5ec90155606b26cb71a2ce1aa")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
     let [<Literal>] AssemblyVersion = "5.0.3"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-27T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-03-31T00:00:00.0000000+01:00"
     let [<Literal>] AssemblyFileVersion = "5.0.3"
     let [<Literal>] AssemblyInformationalVersion = "5.0.3"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha"
-    let [<Literal>] AssemblyMetadata_GitHash = "6a88efe1225969c49050289148a4179c3080b0dc"
+    let [<Literal>] AssemblyMetadata_GitHash = "0566eb7adf247cb5ec90155606b26cb71a2ce1aa"
