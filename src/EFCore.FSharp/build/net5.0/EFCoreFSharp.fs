2318d252d64e8fc33d408fde6a58b22d7c8ecb24
Simon Reynolds <github@simonreynolds.ie>
2021-05-08 16:56:34 +0100

    Automatically set design time services

---
+++ src/EFCore.FSharp/build/net5.0/EFCoreFSharp.fs
@@ -0,0 +1,6 @@
+module EFCoreFSharp
+
+    open Microsoft.EntityFrameworkCore.Design
+
+    [<assembly: DesignTimeServicesReference("EntityFrameworkCore.FSharp.EFCoreFSharpServices, EntityFrameworkCore.FSharp")>]
+    do()
