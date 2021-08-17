2018c7101f4f6d30ed589f871a9ffa1742a85549
Simon Reynolds <github@simonreynolds.ie>
2021-08-17 13:46:30 +0100

    Bump version to 5.0.3-beta006
    ## [5.0.3-beta006] - 2021-08-17
    
    ### Fixed
    - Removed `DevelopmentDependency=true` as it was setting `IncludeAssets` to undesirable values when restoring package - https://github.com/efcore/EFCore.FSharp/pull/108
    
    - Fix formatting of Nullable parameters - [@LiteracyFanatic](https://github.com/LiteracyFanatic) - https://github.com/efcore/EFCore.FSharp/pull/106
    - Handle nullablility correctly in snapshot generation - https://github.com/efcore/EFCore.FSharp/pull/107
    - Issues with scaffolded code
    - Improved code generation for contexts scaffolded from an existing database - https://github.com/efcore/EFCore.FSharp/pull/89
    - Correct issue with InsertData operations and 2D arrays
    - Generate valid code in HasData method
    - Fix issue with generated DbContext members in scaffolded code
    - Resolved issue with migrations have a missing unit arg to the Up method if no changes in model
    - Foreign Key constraints now created correctly
    - Fix indentation issue when table has constraints - https://github.com/efcore/EFCore.FSharp/pull/75
    - Constraints now correctly generated - https://github.com/efcore/EFCore.FSharp/pull/72
    - Generated Migrations will now always include the System namespace - https://github.com/efcore/EFCore.FSharp/pull/70
    - Issues with scaffolded code
    - Improved code generation for contexts scaffolded from an existing database - https://github.com/efcore/EFCore.FSharp/pull/89
    - Correct issue with InsertData operations and 2D arrays
    - Generate valid code in HasData method
    - Fix issue with generated DbContext members in scaffolded code
    - Resolved issue with migrations have a missing unit arg to the Up method if no changes in model
    - Foreign Key constraints now created correctly
    - Fix indentation issue when table has constraints - https://github.com/efcore/EFCore.FSharp/pull/75
    - Constraints now correctly generated - https://github.com/efcore/EFCore.FSharp/pull/72
    - Generated Migrations will now always include the System namespace - https://github.com/efcore/EFCore.FSharp/pull/70
    [Unreleased]: https://github.com/efcore/EFCore.FSharp/compare/v5.0.3-beta005...HEAD
    [5.0.3-beta005]: https://github.com/efcore/EFCore.FSharp/releases/tag/v5.0.3-beta005
    
    ### Added
    - Single case union support - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/98
    - Query translation for Option types - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/93
    - DbSet/IQueryable helpers to deal with EF Core async and nullable methods - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/94
    - Automatic registration of DesignTimeServices - https://github.com/efcore/EFCore.FSharp/pull/86
    - DbContextHelpers - curried functions for interacting with DbContext to allow for a more 'native' F# experience
    - Single case union support - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/98
    - Query translation for Option types - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/93
    - DbSet/IQueryable helpers to deal with EF Core async and nullable methods - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/94
    - Automatic registration of DesignTimeServices - https://github.com/efcore/EFCore.FSharp/pull/86
    - DbContextHelpers - curried functions for interacting with DbContext to allow for a more 'native' F# experience

--- src/EFCore.FSharp/AssemblyInfo.fs
+++ src/EFCore.FSharp/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyVersionAttribute("5.0.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-08-09T00:00:00.0000000+01:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-08-17T00:00:00.0000000+01:00")>]
 [<assembly: AssemblyFileVersionAttribute("5.0.3")>]
 [<assembly: AssemblyInformationalVersionAttribute("5.0.3")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","eb2814661d2e6812f74a6540696e04d0d2631e11")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","640a596f45adf4a9c406461f1422db072ce9bb4b")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
     let [<Literal>] AssemblyVersion = "5.0.3"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-08-09T00:00:00.0000000+01:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-08-17T00:00:00.0000000+01:00"
     let [<Literal>] AssemblyFileVersion = "5.0.3"
     let [<Literal>] AssemblyInformationalVersion = "5.0.3"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
-    let [<Literal>] AssemblyMetadata_GitHash = "eb2814661d2e6812f74a6540696e04d0d2631e11"
+    let [<Literal>] AssemblyMetadata_GitHash = "640a596f45adf4a9c406461f1422db072ce9bb4b"
