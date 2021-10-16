91313981be2fa179cac6e8515cecf46b48dcd866
Simon Reynolds <github@simonreynolds.ie>
2021-10-16 14:59:42 +0100

    Translate calls to isNull to evaluate as IS NULL

---
+++ src/EFCore.FSharp/Translations/IsNullTranslation.fs
@@ -0,0 +1,18 @@
+module EntityFrameworkCore.FSharp.Translations.IsNullTranslation
+
+open EntityFrameworkCore.FSharp
+open Microsoft.EntityFrameworkCore.Query
+
+let isNullMethodTranslator(sqlExp: ISqlExpressionFactory) = {
+    new IMethodCallTranslator with
+    member _.Translate(instance, method, arguments, logger) =
+       if method.DeclaringType.IsValueType then
+            null
+       else
+
+       let expression = arguments |> Seq.head
+       match method.Name with
+       | "IsNull"-> sqlExp.IsNull(expression) :> _
+       | _ -> null
+}
+
