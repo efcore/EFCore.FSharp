ed4ca95007462852503aef0bb925a28a246fe302
Simon Reynolds <github@simonreynolds.ie>
2021-11-13 16:34:17 +0000

    Bump version to 6.0.2
    ## [6.0.2] - 2021-11-13
    
    ### Fixed
    - Fix compilation errors in scaffolded Many-to-Many joins - https://github.com/efcore/EFCore.FSharp/pull/119

--- src/EFCore.FSharp/AssemblyInfo.fs
+++ src/EFCore.FSharp/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
-[<assembly: AssemblyVersionAttribute("6.0.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-11-10T00:00:00.0000000+00:00")>]
-[<assembly: AssemblyFileVersionAttribute("6.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("6.0.1")>]
+[<assembly: AssemblyVersionAttribute("6.0.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-11-13T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("6.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("6.0.2")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","afa5eb77ae6577b0be8b00dc2e7481a3528fe1d3")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","59c6e99b220eccbfd4094ca2e3feeb89ba0507a5")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
-    let [<Literal>] AssemblyVersion = "6.0.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-11-10T00:00:00.0000000+00:00"
-    let [<Literal>] AssemblyFileVersion = "6.0.1"
-    let [<Literal>] AssemblyInformationalVersion = "6.0.1"
+    let [<Literal>] AssemblyVersion = "6.0.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-11-13T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "6.0.2"
+    let [<Literal>] AssemblyInformationalVersion = "6.0.2"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "afa5eb77ae6577b0be8b00dc2e7481a3528fe1d3"
+    let [<Literal>] AssemblyMetadata_GitHash = "59c6e99b220eccbfd4094ca2e3feeb89ba0507a5"
