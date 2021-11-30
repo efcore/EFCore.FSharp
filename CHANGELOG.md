6a658a15888ef9f794badf728288ffda8180e9b3
Simon Reynolds <github@simonreynolds.ie>
2021-11-30 16:59:21 +0000

    Bump version to 6.0.5
    ## [6.0.5] - 2021-11-30
    
    ### Fixed
    - AlterColumn operations no longer emit compiler warnings - https://github.com/efcore/EFCore.FSharp/pull/125
    
    ### Known Issues
    - AlterColumn operations are being created in what should be empty migrations. No effect on functionality but shouldn't be happening - associated issue - https://github.com/efcore/EFCore.FSharp/issues/126

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [6.0.5] - 2021-11-30
+
+### Fixed
+- AlterColumn operations no longer emit compiler warnings - https://github.com/efcore/EFCore.FSharp/pull/125
+
+### Known Issues
+- AlterColumn operations are being created in what should be empty migrations. No effect on functionality but shouldn't be happening - associated issue - https://github.com/efcore/EFCore.FSharp/issues/126
+
 ## [6.0.4] - 2021-11-29
 
 ### Fixed
@@ -47,7 +55,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - DbSet/IQueryable helpers to deal with EF Core async and nullable methods - [@lucasteles](https://github.com/lucasteles) - https://github.com/efcore/EFCore.FSharp/pull/94
 - Automatic registration of DesignTimeServices - https://github.com/efcore/EFCore.FSharp/pull/86
 - DbContextHelpers - curried functions for interacting with DbContext to allow for a more 'native' F# experience
-[Unreleased]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.4...HEAD
+[Unreleased]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.5...HEAD
+[6.0.5]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.4...v6.0.5
 [6.0.4]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.3...v6.0.4
 [6.0.3]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.2...v6.0.3
 [6.0.2]: https://github.com/efcore/EFCore.FSharp/compare/v6.0.1...v6.0.2
--- src/EFCore.FSharp/AssemblyInfo.fs
+++ src/EFCore.FSharp/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("EFCore.FSharp")>]
 [<assembly: AssemblyProductAttribute("EFCore.FSharp")>]
-[<assembly: AssemblyVersionAttribute("6.0.4")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-11-29T00:00:00.0000000+00:00")>]
-[<assembly: AssemblyFileVersionAttribute("6.0.4")>]
-[<assembly: AssemblyInformationalVersionAttribute("6.0.4")>]
+[<assembly: AssemblyVersionAttribute("6.0.5")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-11-30T00:00:00.0000000+00:00")>]
+[<assembly: AssemblyFileVersionAttribute("6.0.5")>]
+[<assembly: AssemblyInformationalVersionAttribute("6.0.5")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","2d6c3d39d8207d535d3f80ce8ca34820b0e59ec9")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","9b0e92f0fe99052d899a2c5bc55a5f8b61416127")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "EFCore.FSharp"
     let [<Literal>] AssemblyProduct = "EFCore.FSharp"
-    let [<Literal>] AssemblyVersion = "6.0.4"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-11-29T00:00:00.0000000+00:00"
-    let [<Literal>] AssemblyFileVersion = "6.0.4"
-    let [<Literal>] AssemblyInformationalVersion = "6.0.4"
+    let [<Literal>] AssemblyVersion = "6.0.5"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-11-30T00:00:00.0000000+00:00"
+    let [<Literal>] AssemblyFileVersion = "6.0.5"
+    let [<Literal>] AssemblyInformationalVersion = "6.0.5"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "2d6c3d39d8207d535d3f80ce8ca34820b0e59ec9"
+    let [<Literal>] AssemblyMetadata_GitHash = "9b0e92f0fe99052d899a2c5bc55a5f8b61416127"
